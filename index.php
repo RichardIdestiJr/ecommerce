ef2ea8393c2de8903310f94192b74dbb9d00bfb0
richardidestijr <richardidesti1337@outlook.com>
2018-09-21 13:56:14 -0300

    painel adm tpl adm

--- index.php
+++ index.php
@@ -4,11 +4,13 @@ require_once("vendor/autoload.php");
 
 use Slim\Slim;
 use \hcode\Page;
+use \hcode\PageAdmin;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
+//rota
 $app->get('/', function() {
     
 	$page = new Page();
@@ -16,6 +18,15 @@ $app->get('/', function() {
 
 
 });
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
+	$page->setTpl("index");
+
+
+});
+
 
 $app->run();
 
